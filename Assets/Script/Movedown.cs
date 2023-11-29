c13ffb171e9d2d68a29e479258685e117255d833
Diamond26400 <obaloluwajames5@gmail.com>
2023-11-29 21:20:53 +0100

    Update Movedown.cs

--- Assets/Script/Movedown.cs
+++ Assets/Script/Movedown.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public float speed;
-[SerializeField] Rigidbody objectRb;
+   private Rigidbody objectRb;
     private float Zdestroy = 10.0f;
     // Start is called before the first frame update
     void Start()
@@ -16,7 +16,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        objectRb.AddForce(Vector3.forward * speed);
+        objectRb.AddForce(Vector3.back * speed);
 
         if (transform.position.z < Zdestroy)
         {
